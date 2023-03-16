e55f6d3eeed59dce070a5098b72dd97923b997f8
Meliodas <banktv76@gmail.com>
2023-03-16 17:38:20 +0700

    Admin_payhotel_page.

--- components/payHotel.tsx
+++ components/payHotel.tsx
@@ -2,32 +2,34 @@ import React from 'react'
 
 export const payHotel = () => {
   return (
-    <div className="flex flex-col items-center">
-    <p className=' font-bold mb-20 text-2xl'>MINT TOKEN</p>
-    <div className=" flex  flex-col">
-        <div className="flex mb-5">
-            <p className='mr-40 text-xl'>Mint: </p>
-            <input type="text" className='text-xl text-center px-2 border border-orange-400 rounded-xl'></input>
-            <p className='ml-12 text-xl'>ADM</p>
-        </div>
-        <div className="flex mb-5">
-            <p className='mr-5 text-xl'>CurrentTotalSupply: </p>
-            <input type="text" className='text-xl text-center px-2 border border-orange-400 rounded-xl'></input>
-            <p className='ml-12 text-xl'>ADM</p>
-        </div>
-        <div className="flex mb-12">
-            <p className='mr-12 text-xl'>NewTotalSupply: </p>
-            <input type="text" className='text-xl text-center px-2 border border-orange-400 rounded-xl'></input>
-            <p className='ml-12 text-xl'>ADM</p>
-        </div>
-        <div className="flex justify-center items-center">
-            <button className='text-xl mr-10 bg-orange-300  rounded-xl px-10 py-2 text-white font-bold '>Cancel</button>
-
-            <button className='text-xl bg-orange-400  rounded-xl px-10 py-2 text-white font-bold '>SUBMIT</button>
-        </div>
-    </div>
-    <div className=""></div>
-    <div className=""></div>
+    <div className="flex flex-col items-center font-work_sans">
+    <p className=' font-bold text-2xl mb-24'>PAY HOTEL</p>
+    <table className='border-separate table-auto text-center'>
+        <thead className=' text-xl'>
+            <tr className=''>
+                <th className=' w-1/3 '>Hotel name</th>
+                <th className='w-1/3 '>Amount (THB)</th>
+                <th className='w-1/3'></th>
+            </tr>
+        </thead>
+        <tbody className=' text-lg'>
+            <tr className=''>
+                <td>Naka Residence Hotel</td>
+                <td className=' text-orange-400'>10</td>
+                <td> <button className=' w-full px-3 py-2 rounded-2xl text-white font-bold bg-orange-400 hover:bg-orange-500'>PAY</button> </td>
+            </tr>
+            <tr>
+                <td>Naka Residence Hotel</td>
+                <td className='text-orange-400'>10</td>
+                <td> <button className=' w-full px-3 py-2 rounded-2xl text-white font-bold bg-orange-400 hover:bg-orange-500'>PAY</button> </td>
+            </tr>
+            <tr>
+                <td>Naka Residence Hotel</td>
+                <td className='text-orange-400'>10</td>
+                <td> <button className='w-full px-3 py-2 rounded-2xl text-white font-bold bg-orange-400 hover:bg-orange-500'>PAY</button> </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 
 
---
+++ pages/admin_page/admin_payHotel_page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PayHotel from '@/components/payHotel'
+
+
+export const admin_payHotel_page = () => {
+  return (
+    <div className=" h-screen font-work_sans flex flex-col justify-center">
+        <PayHotel/>
+</div>
+  )
+}
+
+export default admin_payHotel_page
