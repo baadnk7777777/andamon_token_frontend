e4353653b92aaaeb65698aabde59d3a5855d8ba3
Meliodas <banktv76@gmail.com>
2023-03-16 21:24:50 +0700

    customer buy token page.

---
+++ pages/customer_page/customer_buytoken_page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BUY_token from '@/components/buy_token'
+
+export const customer_buytoken_page = () => {
+  return (
+    <div className=" h-screen font-work_sans flex flex-col justify-center">
+            <BUY_token/>
+        </div>
+  )
+}
+
+export default customer_buytoken_page
